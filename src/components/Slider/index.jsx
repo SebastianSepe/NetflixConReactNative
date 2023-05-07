2f993aeed161b7ddc2af71932c2e0d86d0652960
SebastianSepe <sebastiansepe@outlook.es>
2023-05-07 16:47:59 -0300

    Fix slider

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,34 +1,47 @@
-import { Dimensions, Image, View } from "react-native";
-import { useState } from "react";
+import {
+  Dimensions,
+  Image,
+  View
+} from "react-native";
+import React, { Component } from 'react';
 
-import Swiper from "react-native-swiper";
+import Swiper from 'react-native-swiper';
 import styles from "./styles";
 
-const Slide = ({ uri, width, height }) => (
+const {width} = Dimensions.get('window')
+
+const Slider = props => ( 
   <View style={styles.container}>
-    <Image style={{ width, height }} source={{ uri }} />
+      <Image style={styles.image} source={props.uri}/>
   </View>
-);
+)
 
-export default function Slider() {
-  const { width } = Dimensions.get('window');
-  const [imagesSlider, setImagesSlider] = useState([
-    require("../../assets/imgs/eljuegodelcalamar.jpg"),
-    require("../../assets/imgs/minions.jpg"),
-    require("../../assets/imgs/tarzan.jpg"),
-  ]);
-
-  const handleImageChange = (newImages) => {
-    setImagesSlider(newImages);
-  };
-
-  return (
-    <View style={styles.swiperContainer}>
-      <Swiper autoplay height={180}>
-        {imagesSlider.map((item, i) => (
-          <Slide uri={item} key={i} width={width} height={180} />
-        ))}
-      </Swiper>
-    </View>
-  );
+export default class extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            imagesSlider:  [
+                require('../../assets/imgs/eljuegodelcalamar.jpg'),
+                require('../../assets/imgs/minions.jpg'),
+                require('../../assets/imgs/tarzan.jpg')
+            ]
+        }       
+    }
+    render(){
+        return (
+            <View style={styles.test}>
+              <Swiper
+                  autoplay
+                  height={180}
+                >
+                {
+                  this.state.imagesSlider.map((item, i) => <Slider
+                      uri={item}
+                      key={i}
+                    />)
+                  }
+              </Swiper>
+          </View>
+      )
+  }
 }
--- src/components/Slider/styles.js
+++ src/components/Slider/styles.js
@@ -1,38 +1,18 @@
-import { StyleSheet, Dimensions } from "react-native";
+import { StyleSheet, Dimensions } from "react-native"
 
-const { width, height} = Dimensions.get("window");
-
-
-
+const {width} = Dimensions.get('window')
 const styles = StyleSheet.create({
-  swiperContainer: {
-    flext:1,
-    height: 180,
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  image: {
-    width: width,
-    height: height,
-    resizeMode: 'cover',
-  },
-});
+    container: {
 
+        justifyContent: "center",
+    },
+    image: {
+        width
+    }, 
+    test: {
+        flex: 1,
+    }  
+     
+})
 
 export default styles;
-
-
-// const styles = StyleSheet.create({
-//   container: {
-//     justifyContent: "center",
-//   },
-//   image: {
-//     width,
-//   },
-//   swiperContainer: {
-//     flex: 1,
-//   },
-// });
